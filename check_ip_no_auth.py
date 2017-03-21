147309482dbfc8c4d78d2d149806558922731e94
manuti <manuti@gmail.com>
2017-03-21 11:30:02 +0100

    Small change in the email config section
    Small change in the email config section.

--- check_ip_no_auth.py
+++ check_ip_no_auth.py
@@ -12,9 +12,9 @@ import smtp, string
 ################################################
 FIXED_IP = '10.0.1.2'
 IP_FILEPATH = '/home/pi/current_ip.txt'
-SMTP_USERNAME = 'feersumendjinn@gmail.com'
-SMTP_PASSWORD = 'password'
-SMTP_RECIPIENT = 'feersumendjinn@gmail.com'
+SMTP_USERNAME = 'YOUR_EMAIL_ADDRESS@gmail.com'
+SMTP_PASSWORD = 'YOUR_PASSWORD'
+SMTP_RECIPIENT = 'YOUR_EMAIL_ADDRESS@gmail.com'
 SMTP_SERVER = 'smtp.gmail.com'
 SSL_PORT = 465
 ################################################
